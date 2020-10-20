ccf4c18e790fdc4efea6c6add2ce396761626958
Zelbe04 <u18075020@tuks.co.za>
2020-10-20 14:54:15 +0200

    initial php setup

--- index.php
+++ index.php
@@ -26,7 +26,13 @@
 		    if($value <= $credit ){
 		    	echo "<p>".$key."</p>"; 
 		    }
-	    }
+      }
+      function tax_calc($amount,$tax){
+        $calculate_tax = $amount*$tax;
+        $amount = round($amount+$calculate_tax,2);
+        return $amount;
+      }
+      echo tax_calc(750,0.223);
     ?>
   </body>
 </html>
